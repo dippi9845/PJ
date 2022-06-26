0ae4a845dd232f4f39e0684d308db3fef946afc3
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-06-27 01:51:44 +0200

    Added more check to WebPage

--- src/model/injectable/WebPage.py
+++ src/model/injectable/WebPage.py
@@ -4,6 +4,11 @@ class WebPage:
 
     def __init__(self, URL : str, *injectables : Injectable) -> None:
         self.__URL = URL
+
+        for i in injectables:
+            if not isinstance(Injectable):
+                raise TypeError("The element " + i + " was not an instance of injectable")
+
         self.__injectables = list(injectables)
     
     def __init__(self, URL : str, injectables : list) -> None:
