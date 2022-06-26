8373036ea4d487a8f7f797a2c681960f726eda61
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-06-26 15:47:41 +0200

    Reorganized methos in Variable
    renamed variable that is a confusing name, to isVariable, I think this case is not hte python standard, but anyway for now is called like this

--- src/model/injectable/Variable.py
+++ src/model/injectable/Variable.py
@@ -6,8 +6,23 @@ class Variable(Injectable):
         self.__varName =  varName
         self.__protocol = protocol
         self.__content = content
-        self.__variable = variable
+        self.__isVariable = variable
     
+    def __str__(self):
+        return self.__varName + "=" + self.__content
+    
+    def getVariableName(self):
+        return self.__varName
+    
+    def getProtocol(self):
+        return self.__protocol
+    
+    def isVaraible(self):
+        return self.__isVariable
+    
+    def getContent(self):
+        return self.__content
+
     def _clear(self):
         self.__content = ""
 
@@ -16,5 +31,3 @@ class Variable(Injectable):
             self._clear()
             self.__content = payload
     
-    def __str__(self):
-        return self.__varName + "=" + self.__content
