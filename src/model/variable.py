f5e403ebc348ffd806f7ae4b3468222e51751de9
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-06-30 18:29:55 +0200

    removed module varaible, and created a variable superclass

---
+++ src/model/variable.py
@@ -0,0 +1,24 @@
+'''
+This class model a url a varaible
+'''
+class Variable:
+
+    def __init__(self, var_name : str, protocol : str, content="") -> None:
+        self.__var_name =  var_name
+        self.__protocol = protocol
+        self.__content = content
+    
+    def _set_content(self, content : str) -> None:
+        self.__content = content
+
+    def get_protocol(self) -> str:
+        return self.__protocol
+    
+    def get_variable_name(self) -> str:
+        return self.__var_name
+
+    def get_content(self) -> str:
+        return self.__content
+    
+    def __str__(self) -> str:
+        return self.__var_name + "=" + self.__content
