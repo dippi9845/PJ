f82e0dbfe7bb596f4d5fe52ac9781030ac86f75e
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-07-03 19:51:16 +0200

    renamed injectable into InjectableInterface
    and now is istantaible

--- src/model/injectable/injectable_interface.py
+++ src/model/injectable/injectable_interface.py
@@ -1,10 +1,7 @@
 '''
 An interface that model a whatever thing that can be injected a payload
 '''
-class Injectable:
-
-    def __init__(self) -> None:
-        raise TypeError("This is an interface, so is not instatiable")
+class Injectableinterface:
 
     def _clear():
         pass
