ca83042ac899afb7b1d8fe50444f8c62d68dddfc
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-07-03 19:37:27 +0200

    Update injectable.py
    now is impossible to instatiate an injectable

--- src/model/injectable/injectable.py
+++ src/model/injectable/injectable.py
@@ -3,6 +3,9 @@ An interface that model a whatever thing that can be injected a payload
 '''
 class Injectable:
 
+    def __init__(self) -> None:
+        raise TypeError("This is an interface, so is not instatiable")
+
     def _clear():
         pass
 
