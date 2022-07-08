8cd90f8996386034c3cd09a34043d20119f171dd
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-07-09 01:53:13 +0200

    Update main_view.py
    added log level to main view

--- src/PJ/view/main_view.py
+++ src/PJ/view/main_view.py
@@ -1,16 +1,33 @@
 class MainView:
     
-    def introduction():
+    MAX_LEVEL_OF_LOG = 6
+    WARNING_LOG_LEVEL = 2
+    ERROR_LOG_LEVEL = 1
+
+    def __init__(self, level_of_log=3) -> None:
+        
+        if level_of_log < 1 or level_of_log > self.MAX_LEVEL_OF_LOG:
+            raise TypeError("level of log not valid")
+        
+        self.__level_of_log = level_of_log
+
+    def introduction(self):
         pass
 
-    def ask_input(string : str) -> str:
+    def ask_input(self, string : str) -> str:
         pass
 
-    def log_info():
+    def log(self, string : str):
         pass
 
-    def log_warning():
-        pass
+    def log_info(self, string : str, level_of_log=3):
+        if self.__level_of_log >= level_of_log:
+            self.log(string)
 
-    def log_error():
-        pass
+    def log_warning(self, string : str):
+        if self.__level_of_log >= self.WARNING_LOG_LEVEL:
+            self.log(string)
+
+    def log_error(self, string : str):
+        if self.__level_of_log >= self.ERROR_LOG_LEVEL:
+            self.log(string)
