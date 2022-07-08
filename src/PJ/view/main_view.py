ddea47c0e00cce096acaa4f5a250ae7103eb669b
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-07-09 01:19:41 +0200

    Update main_view.py
    Added body to main_view

--- src/PJ/view/main_view.py
+++ src/PJ/view/main_view.py
@@ -1,2 +1,16 @@
 class MainView:
     
+    def introduction():
+        pass
+
+    def ask_input(string : str) -> str:
+        pass
+
+    def log_info():
+        pass
+
+    def log_warning():
+        pass
+
+    def log_error():
+        pass
