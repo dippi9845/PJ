ddc4a7c748fa0ad489c4656edc2ec7c5fa4b5e6a
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-07-09 01:25:48 +0200

    Create terminal_view.py

---
+++ src/PJ/view/Terminal/terminal_view.py
@@ -0,0 +1,17 @@
+from ..main_view import MainView
+
+class TerminalView(MainView):
+    def introduction():
+        pass
+
+    def ask_input(string : str) -> str:
+        pass
+
+    def log_info():
+        pass
+
+    def log_warning():
+        pass
+
+    def log_error():
+        pass
