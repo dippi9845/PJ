1ed29f5309c2a8eeea420a7761ff71dc751e14ad
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-07-09 02:02:29 +0200

    Update terminal_view.py
    Added some colored text before printing errors or warings

--- src/PJ/view/Terminal/terminal_view.py
+++ src/PJ/view/Terminal/terminal_view.py
@@ -1,8 +1,12 @@
 from ..main_view import MainView
+from colorama import Fore
 
 class TerminalView(MainView):
     
     INTRODUCTION_FILE = "../../../../res/view/introduction.txt"
+
+    WARINING_SEGNALATION = "[" + Fore.YELLOW + "WARINIG" + Fore.WHITE + "] "
+    ERROR_SEGNALATION = "[" + Fore.RED + "ERROR" + Fore.WHITE + "] "
 
     def __init__(self) -> None:
         self.introduction()
@@ -16,3 +20,9 @@ class TerminalView(MainView):
 
     def log(self, string: str, end='\n'):
         print(string, end=end)
+    
+    def log_warning(self, string: str):
+        return super().log_warning(self.WARINING_SEGNALATION + string)
+    
+    def log_error(self, string: str):
+        return super().log_error(self.ERROR_SEGNALATION + string)
