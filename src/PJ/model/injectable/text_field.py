c18821ab460a77b923c4f8201cd9804cb61c4d68
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-07-09 02:27:57 +0200

    added web page and text field from the magic hat

---
+++ src/PJ/model/injectable/text_field.py
@@ -0,0 +1,22 @@
+from injectable import Injectable
+'''
+This class in not instantiable, need to be defined _clear and inject,
+in the controller, couse is up to the controller how to inject the
+payload inside the web page, and how to clear that
+'''
+class TextField(Injectable):
+    def __init__(self, path : str, type : str):
+        self.__path = path
+        self.__type = type
+    
+    def getPath(self):
+        return self.__path
+    
+    def getType(self):
+        return self.__type
+
+    def _clear():
+        pass
+
+    def inject(payload : str):
+        pass
