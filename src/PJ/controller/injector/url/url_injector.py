95f8237cc76d2c8d197713de0738bd1d73f55fff
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-07-09 02:15:04 +0200

    Added url controler part

---
+++ src/PJ/controller/injector/url/url_injector.py
@@ -0,0 +1,8 @@
+from ....model.injectable.injectable import Injectable
+from ....model.injectable.fixed_variable import FixedVariable
+from ....model.injectable.injectable_variable import InjectableVariable
+
+class UrlInjector:
+
+    def __init__(self, url : str, injectables=[], fixed_vars=[], deafault_varaible_fixed=True) -> None:
+        pass
