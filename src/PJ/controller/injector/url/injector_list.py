5507efd3f1375ef6056eb878f50511156b677339
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-07-11 18:48:01 +0200

    Update injector_list.py
    added futher function to create by value

--- src/PJ/controller/injector/url/injector_list.py
+++ src/PJ/controller/injector/url/injector_list.py
@@ -17,3 +17,6 @@ class InjectorList:
 
 def by_values(urls : list[str], payloads : list[list[str]], vars : list[list[InjectableVariable]], vars_in_url_are_fixed=True, fixed_vars=[[]]) -> InjectorList:
     return InjectorList(map(lambda u, p, v, fv: SingleUrlInjector(u,p,v,vars_in_url_are_fixed=vars_in_url_are_fixed, fixed_vars=fv), urls, payloads, vars, fixed_vars))
+
+def by_values(urls : list[str], payloads : list[str], vars : list[list[InjectableVariable]], vars_in_url_are_fixed=True, fixed_vars=[[]]) -> InjectorList:
+    return InjectorList(map(lambda u, v, fv: SingleUrlInjector(u, payloads, v, vars_in_url_are_fixed=vars_in_url_are_fixed, fixed_vars=fv), urls, vars, fixed_vars))
