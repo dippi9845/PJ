714879a32d24b2109ee2a5f823d089803534f88e
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-07-11 18:45:50 +0200

    Update injector_list.py
    added function to create Injectable list by matrix of values

--- src/PJ/controller/injector/url/injector_list.py
+++ src/PJ/controller/injector/url/injector_list.py
@@ -1,4 +1,5 @@
 from single_url_injector import SingleUrlInjector
+from ....model.injectable.injectable_variable import InjectableVariable
 
 class InjectorList:
     def __init__(self, injectors : list[SingleUrlInjector]):
@@ -12,3 +13,7 @@ class InjectorList:
         rtr = [self.__injectors[x:x+num] for x in range(0, len(self.__injectors), num)]
         rtr = map(lambda x: InjectorList(x.copy()), rtr)
         return rtr
+
+
+def by_values(urls : list[str], payloads : list[list[str]], vars : list[list[InjectableVariable]], vars_in_url_are_fixed=True, fixed_vars=[[]]) -> InjectorList:
+    return InjectorList(map(lambda u, p, v, fv: SingleUrlInjector(u,p,v,vars_in_url_are_fixed=vars_in_url_are_fixed, fixed_vars=fv), urls, payloads, vars, fixed_vars))
