ae1f2fe4daf7f10941fcce2a96f1005f812e9c73
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-07-11 18:49:28 +0200

    Update injector_list.py
    added comment for explainations

--- src/PJ/controller/injector/url/injector_list.py
+++ src/PJ/controller/injector/url/injector_list.py
@@ -14,9 +14,14 @@ class InjectorList:
         rtr = map(lambda x: InjectorList(x.copy()), rtr)
         return rtr
 
-
+'''
+    create an Injector list by values, with payloads different by every Single Url Injector
+'''
 def by_values(urls : list[str], payloads : list[list[str]], vars : list[list[InjectableVariable]], vars_in_url_are_fixed=True, fixed_vars=[[]]) -> InjectorList:
     return InjectorList(map(lambda u, p, v, fv: SingleUrlInjector(u,p,v,vars_in_url_are_fixed=vars_in_url_are_fixed, fixed_vars=fv), urls, payloads, vars, fixed_vars))
 
+'''
+    create an Injector list by values, with all the same payloads
+'''
 def by_values(urls : list[str], payloads : list[str], vars : list[list[InjectableVariable]], vars_in_url_are_fixed=True, fixed_vars=[[]]) -> InjectorList:
     return InjectorList(map(lambda u, v, fv: SingleUrlInjector(u, payloads, v, vars_in_url_are_fixed=vars_in_url_are_fixed, fixed_vars=fv), urls, vars, fixed_vars))
