a4a8c05256411c335f0e40c57960b3884d8f39fa
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-07-11 16:00:31 +0200

    renamed url_injector to urls_injector

--- src/PJ/controller/injector/url/urls_injector.py
+++ src/PJ/controller/injector/url/urls_injector.py
@@ -2,7 +2,7 @@ from PJ.controller.injector.url.single_url_injector import SingleUrlInjector
 from ....model.injectable.injectable_variable import InjectableVariable
 from single_url_injector import SingleUrlInjector
 
-class UrlInjector:
+class UrlsInjector:
 
     def __init__(self, urls: list[str], payloads : list[str], vars : list[InjectableVariable], vars_in_url_are_fixed=True, fixed_vars=[]) -> None:
         self.__urls = map(lambda x: SingleUrlInjector(x, payloads, vars, vars_in_url_are_fixed=vars_in_url_are_fixed, fixed_vars=fixed_vars), urls)
