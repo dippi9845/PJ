9845ee8c6e9c3e902e93c09480d49ee2ab766829
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-07-11 15:59:39 +0200

    Moved UrlInjector to SingleUrlInjector
    now an url_injector instatiate a signle url injector for every url

---
+++ src/PJ/controller/injector/url/single_url_injector.py
@@ -0,0 +1,34 @@
+from PJ.model.variable import Variable
+from ....model.injectable.injectable import Injectable
+from ....model.injectable.fixed_variable import FixedVariable
+from ....model.injectable.injectable_variable import InjectableVariable
+from functools import reduce
+from ..url import urls
+
+class SingleUrlInjector:
+
+    def __init__(self, url: str, payloads : list[str], vars : list[InjectableVariable], vars_in_url_are_fixed=True, fixed_vars=[]) -> None:
+        self.__url = url
+        self.__payloads = payloads
+        self.__vars = vars
+        self.__fixed_vars = fixed_vars
+    
+    '''
+    lo farei pure iterabile
+    '''
+
+    def __inject_all_variable(self, payload : str) -> None:
+        for var in self.__vars:
+            var.inject(payload)
+
+    '''
+    Inject all payloads, to a given url
+    '''
+    def inject_all(self):
+        for payload in self.__payloads:
+            self.__inject_all_variable(payload)
+            
+            dicts = map(lambda x: x.to_dict(), self.__vars) + map(lambda x: x.to_dict(), self.__fixed_vars)
+            params = reduce(lambda x,y : x.update(y), dicts)
+
+            urls.url_request(self.url, params)
--- src/PJ/controller/injector/url/url_injector.py
+++ src/PJ/controller/injector/url/url_injector.py
@@ -1,35 +1,13 @@
-from PJ.model.variable import Variable
-from ....model.injectable.injectable import Injectable
-from ....model.injectable.fixed_variable import FixedVariable
+from PJ.controller.injector.url.single_url_injector import SingleUrlInjector
 from ....model.injectable.injectable_variable import InjectableVariable
-from functools import reduce
-from ..url import urls
+from single_url_injector import SingleUrlInjector
 
 class UrlInjector:
 
     def __init__(self, urls: list[str], payloads : list[str], vars : list[InjectableVariable], vars_in_url_are_fixed=True, fixed_vars=[]) -> None:
-        self.__urls = urls
-        self.__payloads = payloads
-        self.__vars = vars
-        self.__fixed_vars = fixed_vars
+        self.__urls = map(lambda x: SingleUrlInjector(x, payloads, vars, vars_in_url_are_fixed=vars_in_url_are_fixed, fixed_vars=fixed_vars), urls)
     
     '''
     lo farei pure iterabile
     '''
-
-    def __inject_all_variable(self, payload : str) -> None:
-        for var in self.__vars:
-            var.inject(payload)
-
-    '''
-    Inject all payloads, to a given url
-    '''
-    def inject_all(self, url : str):
-        for payload in self.__payloads:
-            self.__inject_all_variable(payload)
-            
-            dicts = map(lambda x: x.to_dict(), self.__vars) + map(lambda x: x.to_dict(), self.__fixed_vars)
-            params = reduce(lambda x,y : x.update(y), dicts)
-
-            urls.url_request(url, params)
             
