8d712d0bd3c5dfb0dd3ace5984bb5d6b6b2a18f9
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-07-11 15:50:55 +0200

    Update urls.py
    specified that url is a string

--- src/PJ/controller/injector/url/urls.py
+++ src/PJ/controller/injector/url/urls.py
@@ -2,5 +2,5 @@ from requests import get as get_request
 from requests import Response
 
 
-def url_request(url, params=None) -> Response:
+def url_request(url : str, params=None) -> Response:
     return get_request(url=url, params=params)
