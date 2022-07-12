bc2b2019472543115ff98ef681a4420010357fb7
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-07-12 17:09:56 +0200

    Update option.py
    added same attributes

--- src/PJ/controller/option.py
+++ src/PJ/controller/option.py
@@ -6,4 +6,8 @@ class InjectionType(Enum):
 
 class Option:
     def __init__(self, injection_type : InjectionType, url : str) -> None:
+        self.injection_type = injection_type
+        self.url = url
+        self.variable = []
+        self.fixed_variable = []
         
