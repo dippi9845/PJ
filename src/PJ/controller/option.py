0b3a2aac54a447e6e8a1ce015bb45a4a41e6b91f
Filippo D <filippo.dipietro3@studio.unibo.it>
2022-07-12 22:08:15 +0200

    Update option.py
    created add url method

--- src/PJ/controller/option.py
+++ src/PJ/controller/option.py
@@ -8,6 +8,17 @@ class Option:
     def __init__(self, injection_type : InjectionType, url : str) -> None:
         self.injection_type = injection_type
         self.url = url
-        self.variable = []
-        self.fixed_variable = []
+
+        if self.injection_type == InjectionType.URL:
+            self.variable = []
+            self.fixed_variable = []
+
+    def add_url(self, url : str):
+        if type(self.url) == str:
+            self.url = [self.url, url]
         
+        elif type(self.url) == list[str]:
+            self.url.append(url)
+
+        else:
+            raise TypeError("Unsupported url type")
