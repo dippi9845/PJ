7ab184e4b61d5cf408ca590de38953ae30912f31
Filippo Di Pietro <filippodp9845@gmail.com>
2022-07-16 16:37:51 +0200

    Update option.py
    1) replaced all == with is : is more readable.
    2) add a mockup into the constructor in case of Injection type is webdriver
    3) implemented to dict in Option class

--- src/PJ/controller/option.py
+++ src/PJ/controller/option.py
@@ -1,6 +1,5 @@
 from enum import Enum
-from ..model.variable import FixedVariable, InjectableVariable, Variable
-from ..model.variable import from_dict
+from ..model.variable import FixedVariable, InjectableVariable, Variable, from_dict
 from json import loads
 
 class InjectionType(Enum):
@@ -9,19 +8,26 @@ class InjectionType(Enum):
 
 class Option:
     def __init__(self, injection_type : InjectionType, url, payloads : list[str]) -> None:
-        self.injection_type = injection_type
+        self.__injection_type = injection_type
         self.url = url
         self.payloads = payloads
 
-        if self.injection_type == InjectionType.URL:
+        if self.__injection_type is InjectionType.URL:
             self.variable = []
             self.fixed_variable = []
+        
+        elif self.__injection_type is InjectionType.WEBDRIVER:
+            pass
+            
+        else:
+            raise TypeError("Type of injection is not valid, or supported")
+        
 
     def add_url(self, url : str) -> None:
-        if type(self.url) == str:
+        if type(self.url) is str:
             self.url = [self.url, url]
         
-        elif type(self.url) == list:
+        elif type(self.url) is list:
             self.url.append(url)
 
         else:
@@ -29,32 +35,41 @@ class Option:
     
     def add_variable(self, var : Variable) -> None:
         
-        if type(var) == InjectableVariable:
+        if type(var) is InjectableVariable:
             self.variable.append(var)
         
-        elif type(var) == FixedVariable:
+        elif type(var) is FixedVariable:
             self.fixed_variable.append(var)
         
         else:
             raise TypeError("this variable is not supported")
     
     def to_dict(self) -> dict:
-        pass
+        if self.__injection_type is InjectionType.URL:
+            variable = map(lambda x: x.to_dict(), self.variable)
+            fixed_variable = map(lambda x: x.to_dict(), self.fixed_variable)
+
+            rtr = self.__dict__
+            rtr["variable"] = variable
+            rtr["fixed_variable"] = fixed_variable
+
+        
+        elif self.__injection_type is InjectionType.WEBDRIVER:
+            pass
+            
 
 def by_file(filename : str) -> Option:
     with open(filename, "r") as f:
         data = loads(f.read())
+        tmp = Option(data["injection_type"], data["url"], data["payloads"])
         
-        if data["injection_type"] == InjectionType.URL:
-            tmp = Option(data["injection_type"], data["url"], data["payloads"])
+        if int(data["injection_type"]) is InjectionType.URL.value:
             
             tmp.variable = map(from_dict, data["variable"])
             tmp.fixed_variable = map(from_dict, data["fixed_variable"])
 
             return tmp
         
-        elif data["injection_type"] == InjectionType.WEBDRIVER:
+        elif int(data["injection_type"]) is InjectionType.WEBDRIVER.value:
             pass
         
-        else:
-            raise TypeError("value of injection_type is not recognised")
