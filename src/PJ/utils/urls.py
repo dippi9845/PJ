c02810635c5f28e02d6422ab1d9f93d3bc721536
Filippo Di Pietro <filippodp9845@gmail.com>
2022-07-16 18:06:04 +0200

    Update urls.py
    added method for removing queryies

--- src/PJ/utils/urls.py
+++ src/PJ/utils/urls.py
@@ -1,6 +1,6 @@
 from requests import get as get_request
 from requests import Response
-from urllib.parse import urlparse, ParseResult, parse_qsl
+from urllib.parse import urlparse, parse_qsl, urljoin
 
 def url_request(url : str, params=None) -> Response:
     return get_request(url=url, params=params)
@@ -10,3 +10,6 @@ def url_parameters(url : str) -> dict:
     Returns a dict containing all parameters in a dict
     '''
     return dict(parse_qsl(urlparse(url).query))
+
+def remove_query(url : str) -> str:
+    return urljoin(url, urlparse(url).path)
