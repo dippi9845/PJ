0f927b3657712551d418ae4435c1c7b1e69d8a25
Filippo Di Pietro <filippodp9845@gmail.com>
2022-07-16 18:00:28 +0200

    Update urls.py
    added url_parameters

--- src/PJ/utils/urls.py
+++ src/PJ/utils/urls.py
@@ -1,6 +1,12 @@
 from requests import get as get_request
 from requests import Response
-
+from urllib.parse import urlparse, ParseResult, parse_qsl
 
 def url_request(url : str, params=None) -> Response:
     return get_request(url=url, params=params)
+
+def url_parameters(url : str) -> dict:
+    '''
+    Returns a dict containing all parameters in a dict
+    '''
+    return dict(parse_qsl(urlparse(url).query))
