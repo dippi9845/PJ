3f8fc4a3d6f82b4b187f31c4c337a0dfd5495b9d
Filippo Di Pietro <filippodp9845@gmail.com>
2022-07-16 03:29:01 +0200

    Update main_controller.py
    added ask for mutliple method

--- src/PJ/controller/main_controller.py
+++ src/PJ/controller/main_controller.py
@@ -1,3 +1,4 @@
+from PJ.controller.option import InjectionType
 from PJ.view.main_view import MainView
 from option import Option
 from option import by_file as option_by_file
@@ -8,8 +9,23 @@ class MainController:
         self.__view = view
         self.set_option(option)
 
+    def _ask_for_multiple(self, question : str) -> list:
+        tmp = " "
+        rtr = []
+        
+        while tmp is not "":
+            tmp = input(question)
+            rtr.append(tmp)
+        
+        rtr.pop()
+        
+        return rtr
+
+
     def _complete_option(self):
-        pass
+        in_type = self.__view.ask_input("what kind of injection would you like to perform ? \n" + InjectionType.URL.value + " : to perform url injection\n" + InjectionType.WEBDRIVER.value + " : to perform web page injection")
+        in_type = InjectionType(int(in_type))
+
 
     def set_option(self, option : Option) -> None:
         if option == None:
