880595b5a8f1265bbeec0a8c392e2ba374caece7
Filippo Di Pietro <filippodp9845@gmail.com>
2022-07-18 00:55:11 +0200

    Create url.py
    a class that model a generic url

---
+++ src/PJ/model/url.py
@@ -0,0 +1,17 @@
+from ..utils.urls import url_parameters, remove_query as remove_params
+from ..model.variable import FixedVariable, Variable
+class Url:
+    def __init__(self, url : str, injectable_varaible=[], fixed_variable=[] ,vars_in_url_are_fixed=True) -> None:
+        parameters = url_parameters(url)
+        self.__url = remove_params(url)
+        self.__fixed_vars = []
+        self.__variable = []
+
+        if vars_in_url_are_fixed is True:
+            for name, value in zip(parameters):
+                self.__fixed_vars.append(FixedVariable(name, content=value))
+        
+        elif vars_in_url_are_fixed is False:
+            for name, value in zip(parameters):
+                self.__variable.append(Variable(name))
+        
