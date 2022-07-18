15b333c59d0dec4520caa2a45ac7011201789200
Filippo Di Pietro <filippodp9845@gmail.com>
2022-07-18 22:13:32 +0200

    Update configuration.py
    removed old stuff in to_dict.
    removed add_varaible method.
    removed useless imports.
    now if InjectionType is URL, will be instanced an attribute url

--- src/PJ/controller/configuration.py
+++ src/PJ/controller/configuration.py
@@ -1,6 +1,5 @@
-from cProfile import run
 from enum import Enum
-from PJ.model.variable import FixedVariable, InjectableVariable, Variable, from_dict
+from PJ.model.variable import from_dict
 from json import loads
 from PJ.model.url import Url
 
@@ -23,34 +22,19 @@ class Configuration:
     def __init__(self, config_name : str="Default Config", injection_type : InjectionType=InjectionType.URL, url : list[Url]=[], payloads : list[str]=[]) -> None:
         self.config_name = config_name
         self.injection_type = injection_type
-        self.url = url
         self.payloads = payloads
+        
+        if self.injection_type is InjectionType.URL:
+            self.url = url
         
 
     def add_url(self, url : str) -> None:
         self.url.append(url)
     
-    def add_variable(self, var : Variable) -> None:
-        
-        if type(var) is InjectableVariable:
-            self.variable.append(var)
-        
-        elif type(var) is FixedVariable:
-            self.fixed_variable.append(var)
-        
-        else:
-            raise TypeError("this variable is not supported")
-    
     def to_dict(self) -> dict:
         if self.injection_type is InjectionType.URL:
-            variable = map(lambda x: x.to_dict(), self.variable)
-            fixed_variable = map(lambda x: x.to_dict(), self.fixed_variable)
 
-            rtr = self.__dict__
-            rtr["variable"] = variable
-            rtr["fixed_variable"] = fixed_variable
-
-            return rtr
+            return
 
         elif self.injection_type is InjectionType.WEBDRIVER:
             pass
