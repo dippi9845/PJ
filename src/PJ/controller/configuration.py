79426bdea03b6fd24463fa2789fa2c998b3a0d15
Filippo Di Pietro <filippodp9845@gmail.com>
2022-07-18 23:36:13 +0200

    Rebased configuration.py
    Configuration class is not istantiable, so is open to extension.
    an extension is UrlConfiguration specified for url injection.
    added possibility to add payloads by file.
    function for create configuration by file is temporarly empty

--- src/PJ/controller/configuration.py
+++ src/PJ/controller/configuration.py
@@ -2,56 +2,62 @@ from enum import Enum
 from PJ.model.variable import from_dict
 from json import loads
 from PJ.model.url import Url
-
-'''
-Da rifare tutto:
-    option ora contiene:
-        1) i vari file dei payload
-        2) i vari file dove trovare gli oggetti da iniettare Url
-        3) quanti threa creare per l'iniezione
-    
-    option deve essere esportabile via file
-
-'''
+from injector.url.injector_list import InjectorList
 
 class InjectionType(Enum):
     URL = "1"
     WEBDRIVER = "2"
 
 class Configuration:
-    def __init__(self, config_name : str="Default Config", injection_type : InjectionType=InjectionType.URL, url : list[Url]=[], payloads : list[str]=[]) -> None:
+    def __init__(self, config_name : str="Default Config", payloads : list[str]=[], payload_files : list[str] = [], payload_file_separetor : str="\n") -> None:
         self.config_name = config_name
-        self.injection_type = injection_type
+        self.payload_files = payload_files
+        self.payload_files_to_add = payload_files
+        self.payload_file_separetor = payload_file_separetor
         self.payloads = payloads
-        
-        if self.injection_type is InjectionType.URL:
-            self.url = url
-        
 
+        self.load_payload_file()
+    
+    def add_payload_file(self, payload_file : str) -> None:
+        self.payload_files_to_add.append(payload_file)
+
+    def load_payload_file(self) -> None:
+        for i in self.payload_files_to_add:
+            with open(i, "r") as f:
+                self.payloads += f.read().split(self.payload_file_separetor)
+        
+        self.payload_files_to_add = []
+    
+    def build_injector(self):
+        raise NotImplementedError("build_injetor() method need to be implemented in subclasses")
+
+    def to_dict(self) -> dict:
+        return {"Injection Type": "Not defined", "Name" : self.config_name, "Payloads" : self.payloads, "Payload Files" : self.payload_files}
+
+
+class UrlConfiguration(Configuration):
+    
+    def __init__(self, config_name: str = "Default Config", payloads: list[str] = [], payload_files: list[str] = [], url : list[Url] = []) -> None:
+        super().__init__(config_name, payloads, payload_files)
+        self.url = url
+    
     def add_url(self, url : str) -> None:
         self.url.append(url)
     
+    def build_injector(self) -> InjectorList:
+        raise NotImplementedError("build_injetor() not yet implemented")
+    
     def to_dict(self) -> dict:
-        if self.injection_type is InjectionType.URL:
-
-            return
-
-        elif self.injection_type is InjectionType.WEBDRIVER:
-            pass
-            
+        return super().to_dict().update({"Injection Type": InjectionType.URL.value, "Urls" : [i.to_dict() for i in self.url]})
+        
 
 def by_file(filename : str) -> Configuration:
     with open(filename, "r") as f:
         data = loads(f.read())
-        tmp = Configuration(data["injection_type"], data["url"], data["payloads"])
         
         if data["injection_type"] is InjectionType.URL.value:
-            
-            tmp.variable = map(from_dict, data["variable"])
-            tmp.fixed_variable = map(from_dict, data["fixed_variable"])
-
-            return tmp
+            pass
         
         elif data["injection_type"] is InjectionType.WEBDRIVER.value:
-            pass
+            raise NotImplementedError("Web driver as injection type is not implemented yet")
         
