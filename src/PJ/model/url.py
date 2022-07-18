0c52d0d98b7b54666c5eda855a8afd9396c4a748
Filippo Di Pietro <filippodp9845@gmail.com>
2022-07-18 17:29:24 +0200

    Update url.py
    added function toinject a payload in all varaible in the url, and defined __str__ method

--- src/PJ/model/url.py
+++ src/PJ/model/url.py
@@ -1,7 +1,9 @@
-from ..utils.urls import url_parameters, remove_query as remove_params
-from ..model.variable import FixedVariable, Variable
+from ..utils.urls import url_parameters, remove_query as remove_params, unparse_url
+from ..model.variable import FixedVariable, InjectableVariable
+from functools import reduce
+
 class Url:
-    def __init__(self, url : str, injectable_varaible=[], fixed_variable=[] ,vars_in_url_are_fixed=True) -> None:
+    def __init__(self, url : str, injectable_varaible : list[InjectableVariable]=[], fixed_variable : list[FixedVariable]=[] ,vars_in_url_are_fixed=True) -> None:
         parameters = url_parameters(url)
         self.__url = remove_params(url)
         self.__fixed_vars = fixed_variable
@@ -13,5 +15,13 @@ class Url:
         
         elif vars_in_url_are_fixed is False:
             for name, value in zip(parameters):
-                self.__variable.append(Variable(name))
-        
+                self.__variable.append(InjectableVariable(name))
+    
+    def inject(self, payload : str) -> None:
+        for i in self.__variable:
+            i.inject(payload)
+    
+    def __str__(self) -> str:
+        params = map(lambda x: x.to_dict(), self.__variable) + map(lambda x: x.to_dict(), self.__fixed_vars)
+        params = reduce(lambda x,y : x.update(y), params)
+        return unparse_url(self.__url, params)
