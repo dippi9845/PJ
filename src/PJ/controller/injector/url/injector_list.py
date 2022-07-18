a5076678feddd36c5262c4e139183ec17e35d617
Filippo Di Pietro <filippodp9845@gmail.com>
2022-07-19 00:37:37 +0200

    Update injector_list.py
    raplaced use of map with list one-line notation.
    removed douplicated methods.
    adepted by_values to SingleurlInjector constructor parameters.
    inject_all now is public

--- src/PJ/controller/injector/url/injector_list.py
+++ src/PJ/controller/injector/url/injector_list.py
@@ -1,5 +1,5 @@
+from PJ.model.url import Url
 from single_url_injector import SingleUrlInjector
-from PJ.model.variable import InjectableVariable
 
 class InjectorList:
     def __init__(self, injectors : list[SingleUrlInjector]):
@@ -8,13 +8,13 @@ class InjectorList:
     def __iter__(self):
         return InjectorListIterator(self)
 
-    def _inject_all(self):
+    def inject_all(self):
         for i in self.__injectors:
             i.inject_all()
     
     def split(self, num : int) -> list:
         rtr = [self.__injectors[x:x+num] for x in range(0, len(self.__injectors), num)]
-        rtr = map(lambda x: InjectorList(x.copy()), rtr)
+        rtr = [InjectorList(x.copy()) for x in rtr]
         return rtr
 
 class InjectorListIterator:
@@ -23,14 +23,9 @@ class InjectorListIterator:
     
     def __iter(self):
         pass
+
 '''
     create an Injector list by values, with payloads different by every Single Url Injector
 '''
-def by_values(urls : list[str], payloads : list[list[str]], vars : list[list[InjectableVariable]], vars_in_url_are_fixed=True, fixed_vars=[[]]) -> InjectorList:
-    return InjectorList(map(lambda u, p, v, fv: SingleUrlInjector(u,p,v,vars_in_url_are_fixed=vars_in_url_are_fixed, fixed_vars=fv), urls, payloads, vars, fixed_vars))
-
-'''
-    create an Injector list by values, with all the same payloads
-'''
-def by_values(urls : list[str], payloads : list[str], vars : list[list[InjectableVariable]], vars_in_url_are_fixed=True, fixed_vars=[[]]) -> InjectorList:
-    return InjectorList(map(lambda u, v, fv: SingleUrlInjector(u, payloads, v, vars_in_url_are_fixed=vars_in_url_are_fixed, fixed_vars=fv), urls, vars, fixed_vars))
+def by_values(urls : list[Url], payloads : list[str]) -> InjectorList:
+    return InjectorList([SingleUrlInjector(x, payloads) for x in urls])
