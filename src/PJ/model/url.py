9b2362a246cd3721bf28ffd9703044e482bb3436
Filippo Di Pietro <filippodp9845@gmail.com>
2022-07-18 19:18:42 +0200

    Update url.py
    added function to get all parameters

--- src/PJ/model/url.py
+++ src/PJ/model/url.py
@@ -1,6 +1,5 @@
 from ..utils.urls import url_parameters, remove_query as remove_params, unparse_url
 from ..model.variable import FixedVariable, InjectableVariable
-from functools import reduce
 
 class Url:
     def __init__(self, url : str, injectable_varaible : list[InjectableVariable]=[], fixed_variable : list[FixedVariable]=[] ,vars_in_url_are_fixed=True) -> None:
@@ -24,6 +23,13 @@ class Url:
         return self.__str__
     
     def __str__(self) -> str:
-        params = map(lambda x: x.to_dict(), self.__variable) + map(lambda x: x.to_dict(), self.__fixed_vars)
-        params = reduce(lambda x,y : x.update(y), params)
-        return unparse_url(self.__url, params)
+        return unparse_url(self.get_url(), self.get_params())
+
+    def get_url(self) -> str:
+        return self.__url
+    
+    def get_params(self) -> dict:
+        vars = map(lambda x: x.to_dict(), self.__variable) + map(lambda x: x.to_dict(), self.__fixed_vars)
+        params = {}
+        map(lambda x : params.update(x), vars)
+        return params
