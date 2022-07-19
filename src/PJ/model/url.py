0dc7bf3f966c43c2fac4097fb69a2757b53d9e55
Filippo Di Pietro <filippodp9845@gmail.com>
2022-07-19 02:02:14 +0200

    Update url.py
    used self.get_injectable_list() and self.get_fixed_list() methods

--- src/PJ/model/url.py
+++ src/PJ/model/url.py
@@ -35,9 +35,8 @@ class Url:
         return self.__url
 
     def get_injectable_dict(self) -> dict:
-        vars = list(map(lambda x: x.to_dict(), self.__variable))
         rtr = {}
-        map(lambda x : rtr.update(x), vars)
+        map(lambda x : rtr.update(x), self.get_injectable_list())
         return rtr
     
     def get_injectable_list(self) -> list:
@@ -47,9 +46,8 @@ class Url:
         return [x.to_dict() for x in self.__fixed_vars]
     
     def get_fixed_dict(self) -> dict:
-        vars = list(map(lambda x: x.to_dict(), self.__fixed_vars))
         rtr = {}
-        map(lambda x : rtr.update(x), vars)
+        map(lambda x : rtr.update(x), self.get_fixed_list())
         return rtr
 
     def get_params(self) -> dict:
