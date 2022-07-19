914a85477034d85903513a08a77a63b2c8752470
Filippo Di Pietro <filippodp9845@gmail.com>
2022-07-19 14:48:34 +0200

    Update main_controller.py
    created separeted method _ask_for_url_injection_type that asks to the user input for url injection type

--- src/PJ/controller/main_controller.py
+++ src/PJ/controller/main_controller.py
@@ -45,32 +45,34 @@ class MainController:
         with open(filename, "r") as f:
             return f.read().split(split_flag)
 
+    def _ask_for_url_injection_type(self):
+        name = self.__view.ask_input("Insert the name of the configuration")
+
+        urls = self._ask_for_url_varaible()
+        
+        ch = self.__view.ask_yes_no("Do you want to insert manually the payloads ? (y/N)")
+        payloads = []
+
+        # TODO : completare
+        if ch is True:
+            pass
+        
+        payload_files = self._ask_for_multiple("Inesert path of file that contais payloads (hit enter to exit)\n")
+        
+        if payload_files is [] and payloads is []:
+            self.__view.log_error("No payload file and no manual payload, provided")
+            self._complete_option()
+        else:
+            return UrlConfiguration(config_name=name, url=urls, payloads=payloads, payload_files=payload_files)
+    
     def _complete_option(self):
         in_type = self.__view.ask_input("what kind of injection would you like to perform ? \n" + InjectionType.URL.value + " : to perform url injection\n" + InjectionType.WEBDRIVER.value + " : to perform web page injection")
         
         in_type = InjectionType(in_type)
 
-        # TODO: magari creare una funzione adatta a fare questo
         if in_type is InjectionType.URL:
-            name = self.__view.ask_input("Insert the name of the configuration")
-
-            urls = self._ask_for_url_varaible()
+            self._ask_for_url_injection_type()
             
-            ch = self.__view.ask_yes_no("Do you want to insert manually the payloads ? (y/N)")
-            payloads = []
-
-            # TODO : completare
-            if ch is True:
-                pass
-            
-            payload_files = self._ask_for_multiple("Inesert path of file that contais payloads (hit enter to exit)\n")
-            
-            if payload_files is [] and payloads is []:
-                self.__view.log_error("No payload file and no manual payload, provided")
-                self._complete_option()
-            else:
-                return UrlConfiguration(config_name=name, url=urls, payloads=payloads, payload_files=payload_files)
-        
         elif in_type is InjectionType.WEBDRIVER:
             raise NotImplementedError("still not implemented")
 
