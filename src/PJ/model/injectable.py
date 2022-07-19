088d5a7b16ff864ed589c27f2ac8c991c9520774
Filippo Di Pietro <filippodp9845@gmail.com>
2022-07-19 04:23:17 +0200

    Update injectable.py
    added raise NotImplementedError instead of pass

--- src/PJ/model/injectable.py
+++ src/PJ/model/injectable.py
@@ -4,7 +4,7 @@ An interface that model a whatever thing that can be injected a payload
 class Injectable:
 
     def _clear():
-        pass
+        raise NotImplementedError("need to be implemented by superclass")
 
     def inject(payload : str):
-        pass
+        raise NotImplementedError("need to be implemented by superclass")
