0db86822013a8c494874c8b8d9dfaa50d8347702
Filippo Di Pietro <filippodp9845@gmail.com>
2022-07-20 02:48:53 +0200

    Create injector.py

---
+++ src/PJ/controller/injector/injector.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+from typing_extensions import Self
+
+class Injector(ABC):
+    
+    @abstractmethod
+    def _get_injection(self, index : int) -> str | Self:
+        pass
+    
+    @abstractmethod
+    def _inject_by_index(self, index : int) -> None:
+        pass
+    
+    @abstractmethod
+    def inject_all(self) -> None:
+        pass
+
+class InjectorIterator:
+    def __init__(self, injector : Injector, injection_num : int) -> None:
+        self.__injector = injector
+        self.__injection_num = injection_num
+        self.__index = 0
+    
+    def _is_over(self) -> bool:
+        self.__index < self.__injection_num
+
+    def _get_injector_at_index(self):
+        return self.__injector._get_injection(self.__index)
+
+    def __next__(self):
+        if self._is_over():
+            injector = self._get_injector_at_index()
+            self.__index += 1
+            return injector
