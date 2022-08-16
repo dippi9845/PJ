6665f406de8fb918f9d07027ccb82bc74259ba99
Filippo Di Pietro <filippodp9845@gmail.com>
2022-08-16 04:00:59 +0200

    Update controller_test.py
    added test for get_url()

--- src/tests/controller_test.py
+++ src/tests/controller_test.py
@@ -15,27 +15,40 @@ class InjectorTests(unittest.TestCase):
         domain = "http://blablabla?k=null&j=null"
         payloads = ["1", "2", "3", "4", "5", "6"]
         
-        url = Url(domain, vars_in_url_are_fixed=False)
+        url1 = Url(domain, vars_in_url_are_fixed=False)
         combinations = list(map(lambda x: domain.replace("null", x), payloads))
         to_call = partial(self.check_by_list_of_combos, combinations)
 
-        injector = UrlInjector(url, payloads, request=to_call)
+        injector = UrlInjector(url1, payloads, request=to_call)
         injector.inject_all()
     
     def test_url_injector_by_iterator(self):
-        domain = "http://blablabla?k=null&j=null"
+        domain = "http://blablabla?ada=null&kl=null"
         payloads = ["1", "2", "3", "4", "5", "6"]
         
-        url = Url(domain, vars_in_url_are_fixed=False)
+        url2 = Url(domain, vars_in_url_are_fixed=False)
         combinations = list(map(lambda x: domain.replace("null", x), payloads))
         to_call = partial(self.check_by_list_of_combos, combinations)
 
-        injector = UrlInjector(url, payloads, request=to_call)
+        injector = UrlInjector(url2, payloads, request=to_call)
         
         for _ in injector:
             pass
         
         self.assertListEqual(combinations, [])
+    
+    def test_get_url(self):
+        domain = "http://blablabla"
+        params = "?giuseppino=null&francesco=null"
+        url3 = Url(f"{domain}{params}")
+        injector = UrlInjector(url3, [])
+
+        self.assertEqual(injector.get_url(), url3.get_url())
+        self.assertEqual(injector.get_url(), domain)
+
+class InjectorListTests(unittest.TestCase):
+    #def 
+    pass
 
 if __name__ is "__main__":
     unittest.main()
