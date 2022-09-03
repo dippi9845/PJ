3473fd6557b1656b944b19b2ba77dd5e88f68730
Filippo Di Pietro <filippodp9845@gmail.com>
2022-09-03 16:14:31 +0200

    Update url_injector.py
    implemented from_dict to_dict for injector interface

--- src/PJ/controller/injector/url_injector.py
+++ src/PJ/controller/injector/url_injector.py
@@ -1,8 +1,21 @@
 from __future__ import annotations
+from enum import Enum
+from urllib import request
 from PJ.model.url import Url
 from PJ.utils.urls import Urls
 from PJ.controller.injector.injector import Injector, InjectorIterator, InjectorList
 from typing import Callable
+
+class ExportUtils(Enum):
+    PAYLOADS = "Payloads"
+    URL = "Url"
+    REUQEST_TYPE = "Request"
+    
+    POST_REQUEST = "POST"
+    GET_REQUEST = "GET"
+    
+    REQUESTS_FUNC = {GET_REQUEST: Urls.get_request, POST_REQUEST : Urls.post_request}
+    FUNC_REQUESTS = {Urls.get_request: GET_REQUEST, Urls.post_request: POST_REQUEST}
 
 class UrlInjector(Injector):
 
@@ -55,6 +68,17 @@ class UrlInjector(Injector):
         for payload in self.__payloads:
             self.__url.inject(payload)
             self.__request(self.__url.get_url(), self.__url.get_params())
+    
+    def to_dict(self):
+        return {
+            ExportUtils.PAYLOADS.value : self.__payloads,
+            ExportUtils.URL.value : self.__url.to_dict(),
+            ExportUtils.REUQEST_TYPE.value: ExportUtils.FUNC_REQUESTS.value[self.__request]
+        }
+    
+    @classmethod
+    def from_dict(cls, dictionary: dict) -> Injector:
+        return cls(Url.from_dict(dictionary[ExportUtils.URL.value]), dictionary[ExportUtils.PAYLOADS.value], request=ExportUtils.REQUESTS_FUNC.value[dictionary[ExportUtils.REUQEST_TYPE.value]])
     
     @classmethod
     def from_values(cls, urls : list[Url], payloads : list[str]) -> InjectorList:
