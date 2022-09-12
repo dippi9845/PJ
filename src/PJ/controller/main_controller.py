4d0afcdf29b2bc9b61b10a8435acb7b180bf85be
Filippo Di Pietro <filippodp9845@gmail.com>
2022-09-12 14:39:23 +0200

    Update main_controller.py
    added functions to interact with configuration

--- src/PJ/controller/main_controller.py
+++ src/PJ/controller/main_controller.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
+from PJ.controller.injector.injector import Injector
 from PJ.model.url import Url
 from PJ.view.main_view import MainView
 from PJ.model.variable import InjectableVariable, FixedVariable
-from PJ.model.configuration import InjectionType, Configuration
+from PJ.model.configuration import Configuration, ExportIdentifier as ConfigurationExport, InjectionType
 from injector.injector import InjectorList
 from typing import Optional
 
@@ -17,6 +18,29 @@ class MainController:
             self.__config = Configuration()
         else:
             self.__config = config
+    
+    def set_config_property(self, key : str, value : str | list | dict) -> None:
+        self.__config[key] = value
+    
+    def add_injector(self, injector : Injector) -> None:
+        self.__config.add_injector(injector)
+
+    def add_global_payload(self, key : InjectionType | str, payload : str) -> None:
+        
+        if type(key) is InjectionType:
+            key = key.value
+        
+        self.__config[ConfigurationExport.GLOBAL_PAYLOADS.value][key].append(payload)
+    
+    def add_global_payload_file(self, key : InjectionType | str, filename : str | list[str]) -> None:
+        
+        if type(key) is InjectionType:
+            key = key.value
+        
+        self.__config.add_payload_file_by_key(key, filename)
+    
+    def load_payloads_from_file(self) -> None:
+        self.__config.load_payload_file()
 
     def start_injecting(self):
         to_inject = self.__config.build_injectors()
