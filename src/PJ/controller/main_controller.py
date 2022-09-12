d4eadf9694de1d02dada8409a7065978a9be24d8
Filippo Di Pietro <filippodp9845@gmail.com>
2022-09-12 17:41:10 +0200

    Update main_controller.py
    implemented main menu

--- src/PJ/controller/main_controller.py
+++ src/PJ/controller/main_controller.py
@@ -1,8 +1,19 @@
 from __future__ import annotations
+from enum import Enum
 from PJ.controller.injector.injector import Injector
+from PJ.controller.injector.url_injector import UrlInjector
 from PJ.view.main_view import MainView
 from PJ.model.configuration import Configuration, InjectionType
 from typing import Optional
+
+class Commands(Enum):
+    ADD_GLOBAL_PAYLOAD = "Add Global Payload"
+    ADD_GLOBAL_PAYLOAD_FILE = "Add Global Payload File"
+    ADD_INJECTOR = "Add Injector"
+    LOAD_PAYLOADS_FROM_FILE = "Load Payloads"
+    START_INJECTING = "Run"
+    INJECT_ALL = "Inject all"
+    EXIT = "Exit"
 
 class MainController:
 
@@ -10,26 +21,92 @@ class MainController:
         self.__view = view
         self.__config = Configuration() if config is None else config
         
-    def set_configuration(self, config : Configuration=None) -> Configuration:
+        self.cmds = {
+            Commands.ADD_GLOBAL_PAYLOAD.value : self.add_global_payload_cmd,
+            Commands.ADD_GLOBAL_PAYLOAD_FILE.value : self.add_global_payload_file_cmd,
+            Commands.ADD_INJECTOR.value : self.add_injector_cmd,
+            Commands.LOAD_PAYLOADS_FROM_FILE.value : self.load_payloads_from_file,
+            Commands.START_INJECTING.value : self.start_injecting,
+            Commands.INJECT_ALL.value : self.inject_all
+        }
+        
+        self.description = {
+            Commands.ADD_GLOBAL_PAYLOAD.value : "add to global list a payload",
+            Commands.ADD_GLOBAL_PAYLOAD_FILE.value : "add a file global list of file that contains payloads",
+            Commands.ADD_INJECTOR.value : "add an injector to the current configuration",
+            Commands.LOAD_PAYLOADS_FROM_FILE.value : "move all payoads from the added global file, to global payloads",
+            Commands.START_INJECTING.value : "start injection with logs",
+            Commands.INJECT_ALL.value : "same as start injection but with no log",
+            Commands.EXIT.value : "for close"
+        }
+        
+        self.injectors = {
+            InjectionType.URL.value : self._build_url_injector
+        }
+        
+        self.__view.introduction()
+        self.main_menu()
+    
+    def main_menu(self):
+        cmds = [x.value for x in Commands]
+        desc = list(map(lambda x: self.description[x], cmds))
+        ch = ""
+        
+        while ch is not Commands.EXIT.value:
+            ch = self.__view.menu("Select the main commands", cmds, desc)
+            self.cmds[ch]()
+        
+    
+    def add_global_payload_cmd(self):
+        types = [InjectionType.URL.value, InjectionType.WEBDRIVER.value]
+        descs = ["Injection by url variables", "Injection by automating the web browser"]
+        
+        key = self.__view.menu("Which kind of payload you want to add", types, descs)
+        paylaod = self.__view.ask_input("Insert the payload")
+        
+        self._add_global_payload(key, paylaod)
+        
+    
+    def add_global_payload_file_cmd(self):
+        types = [InjectionType.URL.value, InjectionType.WEBDRIVER.value]
+        descs = ["Injection by url variables", "Injection by automating the web browser"]
+        
+        key = self.__view.menu("Which kind of payload file you want to add", types, descs)
+        paylaod_file = self.__view.ask_input("Insert the payload file")
+        
+        self._add_global_payload_file(key, paylaod_file)
+    
+    def add_injector_cmd(self):
+        injcetors = [InjectionType.URL.value]
+        desc = ["Injector using url variables"]
+        
+        injector_str = self.__view.menu("Which kind of payload file you want to add", injcetors, desc)
+        injector = self.injectors[injector_str]()
+        self._add_injector(injector)
+    
+    def _build_url_injector(self) -> UrlInjector:
+        pass
+    
+    def _set_configuration(self, config : Configuration=None) -> Configuration:
         if config == None:
             self.__config = Configuration()
         else:
             self.__config = config
     
-    def set_config_property(self, key : str, value : str | list | dict) -> None:
+    def _set_config_property(self, key : str, value : str | list | dict) -> None:
         self.__config[key] = value
     
-    def add_injector(self, injector : Injector) -> None:
+    def _add_injector(self, injector : Injector) -> None:
         self.__config.add_injector(injector)
 
-    def add_global_payload(self, key : InjectionType | str, payload : str | list[str] | set[str]) -> None:
+    def _add_global_payload(self, key : InjectionType | str, payload : str | list[str] | set[str]) -> None:
         
         if type(key) is InjectionType:
             key = key.value
         
         self.__config.add_global_payload(key, payload)
     
-    def add_global_payload_file(self, key : InjectionType | str, filename : str | list[str] | set[str]) -> None:
+    def _add_global_payload_file(self, key : InjectionType | str, filename : str | list[str] | set[str]) -> None:
         
         if type(key) is InjectionType:
             key = key.value
