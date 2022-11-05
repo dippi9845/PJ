7489d15e053d56110a5b600c227aee6e476150db
Filippo Di Pietro <filippodp9845@gmail.com>
2022-11-05 14:43:37 +0100

    Update command_list_view.py
    added add_command  method

--- src/PJ/view/command_list_view.py
+++ src/PJ/view/command_list_view.py
@@ -7,17 +7,25 @@ class CommandListView(MainView):
         super().__init__()
         self.__commands = commands
     
+    def add_command(self, command : str) -> None:
+        self.__commands.append(command)
+    
+    
     def _get_next_command(self) -> str:
         return self.__commands.pop(0)
+    
     
     def introduction(self):
         pass
     
+    
     def ask_input(self, string : str) -> str:
         return self._get_next_command()
     
+    
     def log(self, string : str, end='\n'):
         pass
+    
     
     def ask_for_multiple(self, message: str, elements : list) -> dict:
         rtr = {}
@@ -27,10 +35,12 @@ class CommandListView(MainView):
         
         return rtr
     
+    
     def menu(self, message: str, choices: list[str], descriptions: list[str]) -> str:
         cmd = self._get_next_command()
         if cmd in choices: return cmd
         else: raise ValueError(f"Command {cmd} not valid")
+    
     
     @classmethod
     def from_string(cls, string : str, separetor : str=" ") -> CommandListView:
